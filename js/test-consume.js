909149d585ad220335eb3d383b6a36d953e66e47
Francisco Arroyo <francisco.arroyo@controlzeta.com.mx>
2018-02-12 17:42:18 -0600

    first Quiz

---
+++ js/test-consume.js
@@ -0,0 +1,25 @@
+
+$( document ).ready(function() {
+	console.log( "consume!" );
+	getQuestions();
+});
+	
+function getQuestions(){
+	var html = '<div class="col-md-12"> ' + 
+					'<div class="question-div">';
+		$.each( data.questions, function( i, question ) {
+			
+			html += '<div class="col-md-1 number"> ' +(i+1) + '.- </div> ' + 
+					'<div class="col-md-11">' +
+					'<div class="question"> ' + question.question + ' </div> ' ;
+					if(question.answers){
+					$.each( question.answers, function( i, answer ) {
+							html += '<div class="answer"> ' + answer.answer + ' </div> ' ;
+						});
+					}
+		 	html += '</div> ' ;
+		});
+	html += '</div>';
+	$('#divQuestions').append(html);
+}
+
