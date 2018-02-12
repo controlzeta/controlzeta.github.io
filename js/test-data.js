58e2ec591a5a13351450d0ba668b7d7d7e83aacf
Francisco Arroyo <francisco.arroyo@controlzeta.com.mx>
2018-02-12 15:00:32 -0600

    add test.html

---
+++ js/test-data.js
@@ -0,0 +1,22 @@
+
+var data = {
+	"nombre": "Francisco Arroyo Ortega",
+	"fecNac": "02/06/1987",
+	"titulo": "Lic. en Informática",
+	"escuela": "Instituto Tecnológico de Puebla",
+	"estadoCivil":"Soltero",
+	"empleoActual":"MVC .Net Developer CHUBB",
+	"empleoAnterior":"Full Stack Web Developer X1",
+	"aboutMe" : "Hola Soy Francisco Arroyo un desarrollador web Front End mexicano enfocado a C# por su rubustez, escalabilidad e interoperabilidad con otros sistemas, pero eso no es todo también he trabajado con PHP, Angular JS y VB realizando mejoras, migraciones y reparaciones de sistemas existentes. Puedo trabajar en modo freelance o por consultora.",
+	"database" : "Todo desarrollo dinámico necesita una base de datos para funcionar, mi experiencia en este ambito consiste en creación de bases de datos, scripts, restauraciones, store procedures, optimización de consultas y administración de backups.",
+	"code" : "Mi core de programación es C# con SQL Server, sí lo sé soy un chico Microsoft, pero también me interesan lenguajes de código abierto, algunos de los lenguajes que manejo y me encuentro en proceso de adopción son: Javascript, PHP, NodeJS y Python.",
+	"hobbies" : ".",
+	"questions" :[
+		{ "idioma" : "Español", "dominio" : "Nativo", "progreso" : 100	},
+		{ "idioma" : "Ingles", "dominio" : "Avanzado", "progreso" : 90	},
+		{ "idioma" : "Alemán", "dominio" : "Principiante", "progreso" : 10	},
+		],	
+	"certificaciones" : [{
+		"certificacion" : "Scrum Fundamentals Certified (SFC)"
+	}]
+};
