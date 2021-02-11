02d97b2f886416b76f6c9230d6b31c9a1dbd9e9e
Pako Arroyo <francisco.arroyo@controlzeta.com.mx>
2021-02-11 09:13:34 -0600

    Data change

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ var data = {
 	"estadoCivil":"Soltero",
 	"empleoActual":"Full Stack .Net Developer CHUBB",
 	"empleoAnterior":"Full Stack Web Developer X1",
-	"aboutMe" : "Hola Soy Francisco Arroyo un desarrollador web Full Stack mexicano enfocado a C# por su rubustez, escalabilidad e interoperabilidad con otros sistemas, pero eso no es todo también he trabajado con PHP, Angular JS y VB realizando mejoras, migraciones y reparaciones de sistemas existentes. Puedo trabajar en modo freelance o por consultora.",
+	"aboutMe" : "Hola Soy Francisco Arroyo un desarrollador web Full Stack mexicano enfocado al lenguaje C# por su rubustez, escalabilidad e interoperabilidad con otros sistemas, pero eso no es todo también he trabajado con PHP, Angular JS y VB realizando mejoras, migraciones y reparaciones de sistemas existentes. Puedo trabajar en modo freelance o por consultora.",
 	"database" : "Todo desarrollo dinámico necesita una base de datos para funcionar, mi experiencia en este ambito consiste en creación de bases de datos, scripts, restauraciones, store procedures, optimización de consultas y administración de backups.",
 	"code" : "Mi core de programación es C# con SQL Server, sí lo sé soy un chico Microsoft, pero también me interesan lenguajes de código abierto, algunos de los lenguajes que manejo y me encuentro en proceso de adopción son: Javascript, PHP, NodeJS y Python.",
 	"hobbies" : ".",
